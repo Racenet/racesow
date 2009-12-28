83125df3733d06e29907373122b40f3ed508a6eb
r2 <airdeu@gmail.com>
2009-12-28 15:33:16 +0000

    removed 10° angle pitch for grenades in race

--- source/game/g_weapon.c
+++ source/game/g_weapon.c
@@ -705,7 +705,10 @@ edict_t *W_Fire_Grenade( edict_t *self, vec3_t start, vec3_t angles, int speed, 
 
 	if( aim_up )
 	{
-		angles[PITCH] -= 10; // aim some degrees upwards from view dir
+		// racesow
+		if( !GS_RaceGametype() )
+		// !racesow
+			angles[PITCH] -= 10; // aim some degrees upwards from view dir
 
 		// clamp to front side of the player
 		angles[PITCH] += -90; // rotate to make easier the check
