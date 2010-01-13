00bed59ab78433d05e226747e8f95e01488823bd
weqo <weqo>
2010-01-13 08:31:30 +0000

    Position load will also load the weapon which was used when saved

--- source/game/g_local.h
+++ source/game/g_local.h
@@ -1065,6 +1065,7 @@ typedef struct
 	qboolean position_saved;
 	vec3_t position_origin;
 	vec3_t position_angles;
+	int position_weapon;
 	unsigned int position_lastcmd;
 
 	gsitem_t	*last_drop_item;
