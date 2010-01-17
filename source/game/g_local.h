3c914be1adbbdf352bc1f9f4c561701a90a7ca00
weqo <weqo>
2010-01-17 00:01:53 +0000

    Moved some of the racesow code to g_racesow.c and g_racesow.h. also some initial functions for shooter trigger that isn't compltely done.

--- source/game/g_local.h
+++ source/game/g_local.h
@@ -31,6 +31,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "g_public.h"
 #include "g_syscalls.h"
 #include "g_gametypes.h"
+#include "g_racesow.h"
 
 //==================================================================
 // round(x)==floor(x+0.5f)
@@ -665,6 +666,7 @@ void SP_trigger_counter( edict_t *ent );
 void SP_trigger_elevator( edict_t *ent );
 void SP_trigger_gravity( edict_t *ent );
 
+
 //
 // g_clip.c
 //
@@ -693,7 +695,6 @@ void G_Killed( edict_t *targ, edict_t *inflictor, edict_t *attacker, int damage,
 int G_ModToAmmo( int mod );
 qboolean CheckTeamDamage( edict_t *targ, edict_t *attacker );
 void G_SplashFrac( const vec3_t origin, const vec3_t mins, const vec3_t maxs, const vec3_t point, float maxradius, vec3_t pushdir, float *kickFrac, float *dmgFrac );
-void G_SplashFrac_42( const vec3_t origin, const vec3_t mins, const vec3_t maxs, const vec3_t point, float maxradius, vec3_t pushdir, float *kickFrac, float *dmgFrac );
 void G_TakeDamage( edict_t *targ, edict_t *inflictor, edict_t *attacker, const vec3_t pushdir, const vec3_t dmgdir, const vec3_t point, float damage, float knockback, float stun, int dflags, int mod );
 void G_TakeRadiusDamage( edict_t *inflictor, edict_t *attacker, cplane_t *plane, edict_t *ignore, int mod );
 
