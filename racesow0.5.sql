675e4042d0fc77f805a6b818cbb9215d5acc74c8
zaran <zaran.krleza@gmail.com>
2010-10-03 19:43:38 +0000

    Add player_checkpoint and map_checkpoint tables for checkpoints storing and restoring

--- racesow0.5.sql
+++ racesow0.5.sql
@@ -1,126 +1,154 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.8.1deb5+lenny6
+-- version 3.2.2.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Erstellungszeit: 03. Oktober 2010 um 14:40
--- Server Version: 5.0.51
--- PHP-Version: 5.2.6-1+lenny9
+-- Serveur: localhost
+-- GÃ©nÃ©rÃ© le : Dim 03 Octobre 2010 Ã  21:36
+-- Version du serveur: 5.1.50
+-- Version de PHP: 5.3.3
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Datenbank: `racesow056`
+-- Base de donnÃ©es: `racesow`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `gameserver`
+-- Structure de la table `gameserver`
 --
 
 DROP TABLE IF EXISTS `gameserver`;
 CREATE TABLE `gameserver` (
-  `id` int(11) unsigned NOT NULL auto_increment,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user` varchar(255) NOT NULL,
-  `servername` varchar(255) default NULL,
-  `admin` varchar(255) default NULL,
-  `playtime` bigint(20) unsigned NOT NULL default '0',
-  `races` int(10) unsigned NOT NULL default '0',
-  `maps` int(10) unsigned NOT NULL default '0',
+  `servername` varchar(255) DEFAULT NULL,
+  `admin` varchar(255) DEFAULT NULL,
+  `playtime` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `races` int(10) unsigned NOT NULL DEFAULT '0',
+  `maps` int(10) unsigned NOT NULL DEFAULT '0',
   `created` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `map`
+-- Structure de la table `map`
 --
 
 DROP TABLE IF EXISTS `map`;
 CREATE TABLE `map` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `name` varchar(40) NOT NULL default '',
-  `freestyle` tinyint(1) NOT NULL default '0',
-  `status` enum('enabled','disabled','new','true','false') NOT NULL default 'new',
-  `disabled` enum('true','false') NOT NULL default 'false',
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL DEFAULT '',
+  `freestyle` tinyint(1) NOT NULL DEFAULT '0',
+  `status` enum('enabled','disabled','new','true','false') NOT NULL DEFAULT 'new',
+  `disabled` enum('true','false') NOT NULL DEFAULT 'false',
   `races` mediumint(8) unsigned NOT NULL,
   `playtime` bigint(20) unsigned NOT NULL,
   `weapons` varchar(10) NOT NULL,
-  `created` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`),
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
   KEY `races` (`races`),
   KEY `playtime` (`playtime`),
   KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `player`
+-- Structure de la table `map_checkpoint`
+--
+
+DROP TABLE IF EXISTS `map_checkpoint`;
+CREATE TABLE `map_checkpoint` (
+  `map_id` int(11) NOT NULL,
+  `num` int(11) unsigned NOT NULL,
+  `time` int(11) NOT NULL,
+  PRIMARY KEY (`map_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `player`
 --
 
 DROP TABLE IF EXISTS `player`;
 CREATE TABLE `player` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `name` varchar(64) NOT NULL default '',
-  `simplified` varchar(64) NOT NULL default '',
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL DEFAULT '',
+  `simplified` varchar(64) NOT NULL DEFAULT '',
   `auth_name` varchar(255) NOT NULL,
   `auth_token` varchar(255) NOT NULL,
   `auth_email` varchar(255) NOT NULL,
   `auth_mask` varchar(255) NOT NULL,
   `auth_pass` varchar(255) NOT NULL,
-  `session_token` varchar(255) default NULL,
-  `points` int(11) NOT NULL default '0',
-  `position` mediumint(8) unsigned default NULL,
+  `session_token` varchar(255) DEFAULT NULL,
+  `points` int(11) NOT NULL DEFAULT '0',
+  `position` mediumint(8) unsigned DEFAULT NULL,
   `races` mediumint(8) unsigned NOT NULL,
-  `maps` mediumint(8) unsigned NOT NULL default '0',
+  `maps` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `playtime` bigint(20) NOT NULL,
-  `created` datetime default NULL,
-  PRIMARY KEY  (`id`),
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `points` (`points`),
   KEY `position` (`position`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `player_map`
+-- Structure de la table `player_checkpoint`
+--
+
+DROP TABLE IF EXISTS `player_checkpoint`;
+CREATE TABLE `player_checkpoint` (
+  `player_id` int(11) NOT NULL,
+  `map_id` int(11) NOT NULL,
+  `num` int(11) unsigned NOT NULL,
+  `time` int(11) NOT NULL,
+  PRIMARY KEY (`map_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `player_map`
 --
 
 DROP TABLE IF EXISTS `player_map`;
 CREATE TABLE `player_map` (
   `player_id` int(11) unsigned NOT NULL,
   `map_id` int(11) unsigned NOT NULL,
-  `time` int(11) unsigned default NULL,
-  `races` int(11) unsigned NOT NULL default '0',
-  `points` int(11) unsigned NOT NULL default '0',
-  `playtime` bigint(20) unsigned NOT NULL default '0',
-  `server_id` int(11) default NULL,
-  `created` datetime default NULL,
-  `tries` int(11) default NULL,
-  `duration` bigint(20) default NULL,
-  PRIMARY KEY  (`player_id`,`map_id`)
+  `time` int(11) unsigned DEFAULT NULL,
+  `races` int(11) unsigned NOT NULL DEFAULT '0',
+  `points` int(11) unsigned NOT NULL DEFAULT '0',
+  `playtime` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `server_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `tries` int(11) DEFAULT NULL,
+  `duration` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`player_id`,`map_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `race`
+-- Structure de la table `race`
 --
 
 DROP TABLE IF EXISTS `race`;
 CREATE TABLE `race` (
-  `id` int(11) unsigned NOT NULL auto_increment,
-  `map_id` int(11) unsigned NOT NULL default '0',
-  `player_id` int(11) unsigned NOT NULL default '0',
-  `nick_id` int(11) unsigned NOT NULL default '0',
-  `time` int(11) unsigned NOT NULL default '0',
-  `tries` int(10) unsigned default NULL,
-  `duration` bigint(20) default NULL,
-  `server_id` int(10) unsigned default NULL,
-  `created` datetime NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `map_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `player_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `nick_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `time` int(11) unsigned NOT NULL DEFAULT '0',
+  `tries` int(10) unsigned DEFAULT NULL,
+  `duration` bigint(20) DEFAULT NULL,
+  `server_id` int(10) unsigned DEFAULT NULL,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
