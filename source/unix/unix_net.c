e68cbcbb23ffe860a48c777fd584293fa03b57c9
k1ll <karl.glatzer@gmx.de>
2012-07-01 22:53:17 +0200

    Updated unix.

--- source/unix/unix_net.c
+++ source/unix/unix_net.c
@@ -1,22 +1,22 @@
 /*
-   Copyright (C) 1997-2001 Id Software, Inc.
+Copyright (C) 1997-2001 Id Software, Inc.
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License
-   as published by the Free Software Foundation; either version 2
-   of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-   See the GNU General Public License for more details.
+See the GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
- */
+*/
 
 #include "../qcommon/qcommon.h"
 
@@ -36,27 +36,23 @@
 //=============================================================================
 
 /*
-   ====================
-   Sys_NET_GetLastError
-   ====================
- */
+* Sys_NET_GetLastError
+*/
 net_error_t Sys_NET_GetLastError( void )
 {
 	switch( errno )
 	{
-		case 0:				return NET_ERR_NONE;
-		case ECONNREFUSED:	return NET_ERR_CONNRESET;
-		case EWOULDBLOCK:	return NET_ERR_WOULDBLOCK;
-		case EINPROGRESS:	return NET_ERR_INPROGRESS;
-		default:			return NET_ERR_UNKNOWN;
+	case 0:				return NET_ERR_NONE;
+	case ECONNREFUSED:	return NET_ERR_CONNRESET;
+	case EWOULDBLOCK:	return NET_ERR_WOULDBLOCK;
+	case EINPROGRESS:	return NET_ERR_INPROGRESS;
+	default:			return NET_ERR_UNKNOWN;
 	}
 }
 
 /*
-   ==================
-   Sys_NET_AsyncResolveHostname
-   ==================
- */
+* Sys_NET_AsyncResolveHostname
+*/
 void Sys_NET_AsyncResolveHostname( const char *hostname )
 {
 }
@@ -64,20 +60,16 @@ void Sys_NET_AsyncResolveHostname( const char *hostname )
 //=============================================================================
 
 /*
-   ====================
-   Sys_NET_SocketClose
-   ====================
- */
+* Sys_NET_SocketClose
+*/
 void Sys_NET_SocketClose( socket_handle_t handle )
 {
 	close( handle );
 }
 
 /*
-   ====================
-   Sys_NET_SocketIoctl
-   ====================
- */
+* Sys_NET_SocketIoctl
+*/
 int Sys_NET_SocketIoctl( socket_handle_t handle, long request, ioctl_param_t* param )
 {
 	return ioctl( handle, request, param );
@@ -86,19 +78,15 @@ int Sys_NET_SocketIoctl( socket_handle_t handle, long request, ioctl_param_t* pa
 //===================================================================
 
 /*
-   ====================
-   Sys_NET_Init
-   ====================
- */
+* Sys_NET_Init
+*/
 void Sys_NET_Init( void )
 {
 }
 
 /*
-   ====================
-   Sys_NET_Shutdown
-   ====================
- */
+* Sys_NET_Shutdown
+*/
 void Sys_NET_Shutdown( void )
 {
 }
--- source/unix/unix_xpm.c
+++ source/unix/unix_xpm.c
@@ -20,11 +20,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-
-#if defined __FreeBSD__
 #include <stdint.h>
-#endif
+#include <assert.h>
 
 /*
 	Die Zustaende unseres Automaten.
